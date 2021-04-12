62ba4f411f3b1954d00b6ed7d87e10dce198d695
Shinya <shinya.work0720@gmail.com>
2021-04-12 18:58:34 +0900

    Userログイン機能を実装

---
+++ minimal/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + ((None, {'fields': ('age', )}),) # 管理サイトの編集画面にageを追加
+    list_display = ['username', 'email', 'age']
+
+admin.site.register(CustomUser, CustomUserAdmin)
