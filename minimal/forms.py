d1051ebe1d0a060f6586a2ad0945d7bcdf636cfd
Shinya <shinya.work0720@gmail.com>
2021-04-13 18:07:53 +0900

    ユーザー登録機能を追加

---
+++ minimal/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.models import User
+from .models import CustomUser
+
+class SignUpForm(UserCreationForm):
+    last_name = forms.CharField(
+        max_length=30,
+        required=False,
+        help_text='オプション',
+        label='苗字'
+    )
+
+    first_name = forms.CharField(
+        max_length=30,
+        required=False,
+        help_text='オプション',
+        label='名前'
+    )
+
+    email = forms.EmailField(
+        max_length=254,
+        help_text='必須　有効なメールアドレスを入力してください',
+        label='Eメールアドレス'
+    )
+
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'last_name', 'first_name', 'email', 'password1', 'password2')
--- minimal/views.py
+++ minimal/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
+from .forms import SignUpForm
 
 # Create your views here.
 
@@ -9,3 +11,17 @@ def index(request):
 @login_required # 非ログイン状態→LOGIN_URLに飛ばす
 def home(request):
     return render(request, 'home.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
