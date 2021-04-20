40ffe042b04a7784013a75a894b73fa9a4efa583
Shinya <shinya.work0720@gmail.com>
2021-04-21 01:58:31 +0900

    DEBUGをTrueに変更

--- minimalist/settings.py
+++ minimalist/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
