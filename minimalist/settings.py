cb46a973d17e183424f3b01d450b4a7703c861a1
Shinya <shinya.work0720@gmail.com>
2021-05-07 21:45:11 +0900

    heroku環境にlsを追加

--- minimalist/settings.py
+++ minimalist/settings.py
@@ -6,7 +6,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -14,7 +13,6 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
