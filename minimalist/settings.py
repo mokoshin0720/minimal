54001c55e985f900936f4089bb79f7bdd6556f4d
Shinya <shinya.work0720@gmail.com>
2021-05-07 19:50:31 +0900

    静的ファイルの保存をwhitenoiseに変更

--- minimalist/settings.py
+++ minimalist/settings.py
@@ -145,17 +145,14 @@ if not DEBUG:
     AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
 
-    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400',}
+    # AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+    # AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400',}
 
     # AWS_DEFAULT_ACL = os.environ.get('AWS_DEFAULT_ACL', 'public-read')
-    AWS_LOCATION = 'static'
-    STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-    AWS_S3_FILE_OVERWRITE = False
+    # AWS_LOCATION = 'static'
+    # STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+    # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
     PRODUCTION_ENV = env.bool('PRODUCTION_ENV', default=True)
     if PRODUCTION_ENV:
